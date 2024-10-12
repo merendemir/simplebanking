e10a553df04a6cdb55605472400aec2e2f4503b8
Mustafa Eren DEMİR <meren.demir@ayvos.com>
2024-10-12 10:37:31 +0300

    initial commit

---
+++ src/main/java/com/eteration/simplebanking/model/Transaction.java
@@ -0,0 +1,5 @@
+package com.eteration.simplebanking.model;
+
+public abstract class Transaction {
+	
+}
---
+++ src/main/java/com/eteration/simplebanking/services/AccountService.java
@@ -0,0 +1,5 @@
+package com.eteration.simplebanking.services;
+
+public class AccountService {
+
+}
